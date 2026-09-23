795ea05bcbe0bfbc0013a8f798a5bc0398fd35ef
ethan1205-w <wethan156@gmail.com>
2026-09-23 15:46:57 -0400

    Update app.R

--- apps/midwest_airbnb/app.R
+++ apps/midwest_airbnb/app.R
@@ -1,14 +1,14 @@
 # ISA 401 Midwest Airbnb Chat: ask questions, get SQL, a table, or a chart back
 library(querychat)
 
-con <- DBI::dbConnect(RSQLite::SQLite(), "data/midwest_airbnb.db")
+con = DBI::dbConnect(RSQLite::SQLite(), "data/midwest_airbnb.db")
 
-client <- ellmer::chat_openai(
+client = ellmer::chat_openai(
   model  = "gpt-5.6-luna",
   params = ellmer::params(reasoning_effort = "none")
 )
 
-qc <- querychat::querychat(
+qc = querychat::querychat(
   con, "listings",
   client             = client,
   tools              = c("filter", "query", "visualize"),
